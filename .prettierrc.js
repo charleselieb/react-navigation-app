324d73439fe9d5bbcd569c019dfaee85f7e24eb7
charleselieb <charleselie@gmail.com>
2022-05-04 09:36:55 -0400

    add some settings to prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,8 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: false,
+  quoteProps: 'preserve',
+  tabWidth: 2,
+  printWidth: 80,
 };
