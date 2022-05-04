077d6ad3aea80a9d9e4147c6b5c670ff1a9ad487
charleselieb <charleselie@gmail.com>
2022-05-04 14:06:53 -0400

    add styled components, and vscode settings

---
+++ src/screens/auth/Login.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Login = ({}: Props) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
