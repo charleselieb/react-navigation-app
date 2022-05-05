40ed43ed4c93234c0d0fbd80631cbc423a1d9526
charleselieb <charleselie@gmail.com>
2022-05-05 09:25:52 -0400

    fix import

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -2,9 +2,9 @@ import {Text} from 'react-native';
 import React, {FC, useContext} from 'react';
 import {Container} from 'components/Container';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {AuthParamList} from 'navigation/Routes';
 import {Button} from 'components/Button';
 import {AuthContext} from 'providers/AuthProvider';
+import {AuthParamList} from 'navigation/AuthStack';
 
 interface LoginProps {
   navigation: NativeStackNavigationProp<AuthParamList, 'Login'>;
